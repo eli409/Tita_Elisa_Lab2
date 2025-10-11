56b33ae3924597109b6c9ea8fc85725cfdaa25a5
diana <diana@ELISIQ>
2025-10-11 20:56:43 +0300

    Add project files.

---
+++ Data/Tita_Elisa_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tita_Elisa_Lab2.Models;
+
+namespace Tita_Elisa_Lab2.Data
+{
+    public class Tita_Elisa_Lab2Context : DbContext
+    {
+        public Tita_Elisa_Lab2Context (DbContextOptions<Tita_Elisa_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tita_Elisa_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Tita_Elisa_Lab2.Data;
+using Tita_Elisa_Lab2.Models;
+
+namespace Tita_Elisa_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Tita_Elisa_Lab2.Data.Tita_Elisa_Lab2Context _context;
+
+        public IndexModel(Tita_Elisa_Lab2.Data.Tita_Elisa_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
